abc0a9900016f87f2bde36105136a195c186f48b
er361 <sf7kmmr@gmail.com>
2017-09-29 09:56:18 +0600

    reg fix2

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -151,7 +151,7 @@ class SiteController extends Controller
         }
 
         $response->data = [
-            'error' => 'empty fields orf wrong format',
+            'error' => 'empty fields or wrong format',
             'format' => 'SignupForm[fieldName]'
         ];
     }
